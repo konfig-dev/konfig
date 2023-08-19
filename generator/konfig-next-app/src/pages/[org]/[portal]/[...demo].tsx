a1e984bd8bfe20bdbac8bec657b16d11ba40b24d
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 13:18:39 -0700

    save

--- generator/konfig-next-app/src/pages/[org]/[portal]/[...demo].tsx
+++ generator/konfig-next-app/src/pages/[org]/[portal]/[...demo].tsx
@@ -1,77 +1,74 @@
-import { DemoPortal, PortalState } from "@/components/DemoPortal";
-import { observer } from "mobx-react";
-import { Organization, Portal, Demo, demos } from "@/utils/demos";
-import Head from "next/head";
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
-import { useState } from "react";
+import { DemoPortal, PortalState } from '@/components/DemoPortal'
+import { observer } from 'mobx-react'
+import { Organization, Portal, Demo, demos } from '@/utils/demos'
+import Head from 'next/head'
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import { useState } from 'react'
 import {
   SocialObject,
   generateDemosDataFromGithub,
-} from "@/utils/generate-demos-from-github";
+} from '@/utils/generate-demos-from-github'
+import type { Spec } from 'konfig-lib'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
-    fallback: "blocking",
-  };
-};
+    fallback: 'blocking',
+  }
+}
 
 export const getStaticProps: GetStaticProps<{
-  organization: Organization;
-  portal: Portal;
-  demo: Demo;
-  socials?: SocialObject;
-  mainBranch: string;
+  organization: Organization
+  portal: Portal
+  socials?: SocialObject
+  mainBranch: string
+  demo?: Demo
+  openapi?: Spec['spec']
 }> = async (ctx) => {
   if (!ctx.params?.org || Array.isArray(ctx.params.org)) {
     return {
       notFound: true,
-    };
+    }
   }
 
   if (!ctx.params?.portal || Array.isArray(ctx.params.portal)) {
     return {
       notFound: true,
-    };
+    }
   }
 
-  if (!ctx.params?.demo || Array.isArray(ctx.params.demo)) {
+  if (!ctx.params?.demo || !Array.isArray(ctx.params.demo)) {
     return {
       notFound: true,
-    };
+    }
   }
 
   const props = await generateDemosDataFromGithub({
     orgId: ctx.params.org,
     portalId: ctx.params.portal,
-    demoId: ctx.params.demo,
-  });
+    demoId: ctx.params.demo[0],
+  })
 
-  if (props.result === "error") return { notFound: true };
+  if (props.result === 'error') return { notFound: true }
 
   return {
     props,
-  };
-};
+  }
+}
 
 const Demo = observer(
-  ({
-    organization,
-    portal,
-    demo,
-    mainBranch,
-    socials,
-  }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const { organization, portal, mainBranch, socials } = props
     const [state] = useState(
       new PortalState({
         ...portal,
         portalId: portal.id,
         organizationId: organization.id,
-        demoId: demo.id,
+        demoId: props.demo?.id,
         mainBranch,
         socials,
       })
-    );
+    )
 
     return (
       <>
@@ -80,8 +77,8 @@ const Demo = observer(
         </Head>
         <DemoPortal state={state} />
       </>
-    );
+    )
   }
-);
+)
 
-export default Demo;
+export default Demo
