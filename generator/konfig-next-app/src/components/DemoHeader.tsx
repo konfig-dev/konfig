665792649608533130448107eca9bd1348535717
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 15:56:12 -0700

    put DemoHeader inside mobx observer

--- generator/konfig-next-app/src/components/DemoHeader.tsx
+++ generator/konfig-next-app/src/components/DemoHeader.tsx
@@ -20,92 +20,99 @@ import {
 import { TITLE_OFFSET_PX } from './DemoTitle'
 import { Dispatch, SetStateAction } from 'react'
 import { PortalState } from './DemoPortal'
+import { observer } from 'mobx-react'
 
-export function DemoHeader({
-  opened,
-  setOpened,
-  state,
-  refreshSandbox,
-  sandbox,
-}: {
-  opened: boolean
-  setOpened: Dispatch<SetStateAction<boolean>>
-  state: PortalState
-  refreshSandbox?: () => void
-  sandbox?: boolean
-}) {
-  const theme = useMantineTheme()
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
-  return (
-    <Header height={{ base: 50, md: TITLE_OFFSET_PX }} p="md">
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          height: '100%',
-        }}
-      >
-        <Group spacing={0}>
-          <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-            <Burger
-              opened={opened}
-              onClick={() => setOpened((o) => !o)}
-              size="sm"
-              color={theme.colors.gray[6]}
-              mr="md"
-            />
-          </MediaQuery>
-          <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-            <Title order={5}>{state.portalName}</Title>
-          </MediaQuery>
-        </Group>
-        <Group>
-          <SegmentedControl
-            size="xs"
-            color="blue"
-            value={state.showCode ? 'show-code' : 'hide-code'}
-            data={[
-              { label: 'Show Code', value: 'show-code' },
-              { label: 'Hide Code', value: 'hide-code' },
-            ]}
-            onChange={(value) => {
-              state.setShowCode(value === 'show-code')
-            }}
-          />
-          <ActionIcon
-            variant="default"
-            onClick={() => toggleColorScheme()}
-            size={30}
-          >
-            {colorScheme === 'dark' ? (
-              <IconSun size="1rem" />
-            ) : (
-              <IconMoonStars size="1rem" />
-            )}
-          </ActionIcon>
-          {!sandbox && (
-            <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-              <Button
-                component="a"
-                target="_blank"
-                size="xs"
-                leftIcon={<IconBrandGithub size="1rem" />}
-                href={`https://github.com/${state.organizationId}/${state.portalId}/tree/${state.mainBranch}/demos`}
-                color="gray"
-                variant="default"
-              >
-                Source
-              </Button>
+export const DemoHeader = observer(
+  ({
+    opened,
+    setOpened,
+    state,
+    refreshSandbox,
+    sandbox,
+  }: {
+    opened: boolean
+    setOpened: Dispatch<SetStateAction<boolean>>
+    state: PortalState
+    refreshSandbox?: () => void
+    sandbox?: boolean
+  }) => {
+    const theme = useMantineTheme()
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+    return (
+      <Header height={{ base: 50, md: TITLE_OFFSET_PX }} p="md">
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            height: '100%',
+          }}
+        >
+          <Group spacing={0}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="md"
+              />
             </MediaQuery>
-          )}
-          {refreshSandbox && (
-            <ActionIcon onClick={refreshSandbox} color="green" variant="light">
-              <IconRefresh size="1rem" />
+            <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+              <Title order={5}>{state.portalName}</Title>
+            </MediaQuery>
+          </Group>
+          <Group>
+            <SegmentedControl
+              size="xs"
+              color="blue"
+              value={state.showCode ? 'show-code' : 'hide-code'}
+              data={[
+                { label: 'Show Code', value: 'show-code' },
+                { label: 'Hide Code', value: 'hide-code' },
+              ]}
+              onChange={(value) => {
+                state.setShowCode(value === 'show-code')
+              }}
+            />
+            <ActionIcon
+              variant="default"
+              onClick={() => toggleColorScheme()}
+              size={30}
+            >
+              {colorScheme === 'dark' ? (
+                <IconSun size="1rem" />
+              ) : (
+                <IconMoonStars size="1rem" />
+              )}
             </ActionIcon>
-          )}
-        </Group>
-      </div>
-    </Header>
-  )
-}
+            {!sandbox && (
+              <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+                <Button
+                  component="a"
+                  target="_blank"
+                  size="xs"
+                  leftIcon={<IconBrandGithub size="1rem" />}
+                  href={`https://github.com/${state.organizationId}/${state.portalId}/tree/${state.mainBranch}/demos`}
+                  color="gray"
+                  variant="default"
+                >
+                  Source
+                </Button>
+              </MediaQuery>
+            )}
+            {refreshSandbox && (
+              <ActionIcon
+                onClick={refreshSandbox}
+                color="green"
+                variant="light"
+              >
+                <IconRefresh size="1rem" />
+              </ActionIcon>
+            )}
+          </Group>
+        </div>
+      </Header>
+    )
+  }
+)
