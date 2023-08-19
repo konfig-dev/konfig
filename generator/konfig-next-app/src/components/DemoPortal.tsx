5c56ab701256b3f4daeba0bdaafca78a78bfb6fe
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 10:01:09 -0700

    remove MadeWithKonfig

--- generator/konfig-next-app/src/components/DemoPortal.tsx
+++ generator/konfig-next-app/src/components/DemoPortal.tsx
@@ -22,6 +22,7 @@ import {
   Divider,
   rem,
   Aside,
+  ScrollArea,
 } from '@mantine/core'
 import {
   IconBug,
@@ -188,38 +189,6 @@ export const DemoPortal = observer(
             </HoverCard>
           </Affix>
         )}
-        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-          <Affix position={{ bottom: '1rem', left: '1rem' }}>
-            <Button
-              variant={colorScheme === 'light' ? 'white' : undefined}
-              size="xs"
-              component="a"
-              href="https://apidemo.konfigthis.com"
-              sx={{
-                ...(colorScheme === 'light'
-                  ? {
-                      color: 'black',
-                      boxShadow:
-                        '0 0 0 1px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.1)',
-                    }
-                  : {
-                      ':hover': {
-                        backgroundColor: '#151515',
-                      },
-                      color: '#e3e3e3',
-                      backgroundColor: '#151515',
-                      borderColor: 'rgba(255,255,255,.15)',
-                    }),
-              }}
-              leftIcon={
-                <Image alt="Logo" width={20} height={20} src="/logo.png" />
-              }
-              target="_blank"
-            >
-              Made with Konfig
-            </Button>
-          </Affix>
-        </MediaQuery>
         <AppShell
           styles={{
             main: {
@@ -240,7 +209,7 @@ export const DemoPortal = observer(
               <Navbar.Section>
                 <NavbarSelect />
               </Navbar.Section>
-              <Navbar.Section grow mt="xs" mb={rem(35)}>
+              <Navbar.Section grow mt="xs" component={ScrollArea}>
                 <Stack spacing="xs">
                   {state.demos.map(({ name }, i) => {
                     const isCurrentlySelected = state.currentDemoIndex === i
