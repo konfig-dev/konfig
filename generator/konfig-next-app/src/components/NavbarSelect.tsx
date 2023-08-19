fc3603e227e21e923bfeb26b9f152e6dd579bd08
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 11:08:15 -0700

    format

--- generator/konfig-next-app/src/components/NavbarSelect.tsx
+++ generator/konfig-next-app/src/components/NavbarSelect.tsx
@@ -1,24 +1,64 @@
-import { Select } from '@mantine/core'
+import { Select, Group, Text } from '@mantine/core'
 
 import { IconCode, IconApi } from '@tabler/icons-react'
-import { useState } from 'react'
+import { makeAutoObservable } from 'mobx'
+import { observer } from 'mobx-react'
+import { forwardRef } from 'react'
 
 const NAVBAR_OPTIONS = {
-  apiReference: { value: 'API Reference', Icon: IconApi },
-  demos: { value: 'Demos', Icon: IconCode },
+  'api-reference': {
+    value: 'api-reference',
+    Icon: <IconApi size="1rem" />,
+    label: 'API Reference',
+  },
+  demos: { value: 'demos', Icon: <IconCode size="1rem" />, label: 'Demos' },
 } as const
 
-export function NavbarSelect() {
-  const [{ value: navigationValue, Icon: NavigationIcon }, setNavigation] =
-    useState<(typeof NAVBAR_OPTIONS)['apiReference' | 'demos']>(
-      NAVBAR_OPTIONS['demos']
-    )
+type ModeType = keyof typeof NAVBAR_OPTIONS
+type Mode = (typeof NAVBAR_OPTIONS)[ModeType]
+class NavbarState {
+  modeType: ModeType = 'demos'
+  constructor() {
+    makeAutoObservable(this)
+  }
 
+  setModeType(modeType: string) {
+    if (modeType === 'api-reference' || modeType === 'demos')
+      this.modeType = modeType
+    else throw Error('Test')
+  }
+
+  get mode(): Mode {
+    return NAVBAR_OPTIONS[this.modeType]
+  }
+}
+
+export const navbarState = new NavbarState()
+
+const SelectItem = forwardRef<HTMLDivElement, Mode>(
+  ({ label, Icon, ...others }: Mode, ref) => (
+    <div ref={ref} {...others}>
+      <Group noWrap>
+        {Icon}
+        <div>
+          <Text size="sm">{label}</Text>
+        </div>
+      </Group>
+    </div>
+  )
+)
+SelectItem.displayName = 'SelectItem'
+
+export const NavbarSelect = observer(() => {
   return (
     <Select
-      data={['API Reference', 'Demos']}
-      value={navigationValue}
-      icon={<NavigationIcon size="1rem" />}
+      data={Object.values(NAVBAR_OPTIONS)}
+      value={navbarState.mode.value}
+      icon={navbarState.mode.Icon}
+      itemComponent={SelectItem}
+      onChange={(value) =>
+        value !== null ? navbarState.setModeType(value) : null
+      }
     />
   )
-}
+})
