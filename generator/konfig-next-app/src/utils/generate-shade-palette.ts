ca080ebbedc76e3af0e2e2bffd96461a24e23c87
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 23:32:16 -0700

    use shade palette generator from https://smart-swatch.netlify.app/#1d344e

--- generator/konfig-next-app/src/utils/generate-shade-palette.ts
+++ generator/konfig-next-app/src/utils/generate-shade-palette.ts
@@ -1,18 +1,34 @@
 import { Tuple } from '@mantine/core'
 import chroma from 'chroma-js'
 
+const LIGHTNESS_MAP = [
+  0.95, 0.85, 0.75, 0.65, 0.55, 0.45, 0.35, 0.25, 0.15, 0.05,
+]
+const SATURATION_MAP = [0.32, 0.16, 0.08, 0.04, 0, 0, 0.04, 0.08, 0.16, 0.32]
+const HUE_MAP = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36]
+
 export const generateShadePalette = (color: string): Tuple<string, 10> => {
-  const baseColor = chroma(color)
-  const lightness = baseColor.get('hsl.l')
-  const targetIndex = Math.round(lightness * 9)
-  const scale = chroma
-    .scale([baseColor.brighten(1.5), baseColor, baseColor.darken(1.5)])
-    .mode('lab')
-  const shades = Array.from({ length: 10 }, (_, i) => {
-    if (i === targetIndex) {
-      return baseColor.hex()
-    }
-    return scale(i / 9).hex()
-  })
-  return shades as Tuple<string, 10>
+  const colorChroma = chroma(color)
+
+  const lightnessGoal = colorChroma.get('hsl.l')
+  const closestLightness = LIGHTNESS_MAP.reduce((prev, curr) =>
+    Math.abs(curr - lightnessGoal) < Math.abs(prev - lightnessGoal)
+      ? curr
+      : prev
+  )
+
+  const baseColorIndex = LIGHTNESS_MAP.findIndex((l) => l === closestLightness)
+
+  const colors = LIGHTNESS_MAP.map((l) => colorChroma.set('hsl.l', l))
+    .map((color) => chroma(color))
+    .map((color, i) => {
+      const saturationDelta = SATURATION_MAP[i] - SATURATION_MAP[baseColorIndex]
+      return saturationDelta >= 0
+        ? color.saturate(saturationDelta)
+        : color.desaturate(saturationDelta * -1)
+    })
+
+  console.log(colors.map((color) => color.hex()))
+
+  return colors.map((color) => color.hex()) as Tuple<string, 10>
 }
