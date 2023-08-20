7c132870e567efc414ce7d98bd44af8636298bcf
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 23:59:30 -0700

    polish colors

--- generator/konfig-next-app/src/utils/generate-shade-palette.ts
+++ generator/konfig-next-app/src/utils/generate-shade-palette.ts
@@ -1,11 +1,7 @@
 import { Tuple } from '@mantine/core'
 import chroma from 'chroma-js'
-
-const LIGHTNESS_MAP = [
-  0.95, 0.85, 0.75, 0.65, 0.55, 0.45, 0.35, 0.25, 0.15, 0.05,
-]
+const LIGHTNESS_MAP = [0.97, 0.94, 0.9, 0.85, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3]
 const SATURATION_MAP = [0.32, 0.16, 0.08, 0.04, 0, 0, 0.04, 0.08, 0.16, 0.32]
-const HUE_MAP = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36]
 
 export const generateShadePalette = (color: string): Tuple<string, 10> => {
   const colorChroma = chroma(color)
@@ -27,8 +23,6 @@ export const generateShadePalette = (color: string): Tuple<string, 10> => {
         ? color.saturate(saturationDelta)
         : color.desaturate(saturationDelta * -1)
     })
-
-  console.log(colors.map((color) => color.hex()))
 
   return colors.map((color) => color.hex()) as Tuple<string, 10>
 }
