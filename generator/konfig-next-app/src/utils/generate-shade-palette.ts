9d58a69335af12bcbd3a43220675331c581d355b
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 22:58:54 -0700

    add generate-shade-palette

---
+++ generator/konfig-next-app/src/utils/generate-shade-palette.ts
@@ -0,0 +1,16 @@
+import { Tuple } from '@mantine/core'
+import chroma from 'chroma-js'
+
+export const generateShadePalette = (color: string): Tuple<string, 10> => {
+  const baseColor = chroma(color)
+  const scale = chroma
+    .scale([baseColor.brighten(1.5), baseColor, baseColor.darken(1.5)])
+    .mode('lab')
+  const shades = Array.from({ length: 10 }, (_, i) => {
+    if (i === 4) {
+      return baseColor.hex()
+    }
+    return scale(i / 9).hex()
+  })
+  return shades as Tuple<string, 10>
+}
