50710f8f18539b8c79567eb53c20d93180717d66
dphuang2 <dylan.p.huang@gmail.com>
2023-08-19 23:13:28 -0700

    smarter generation of shade palette

--- generator/konfig-next-app/src/utils/generate-shade-palette.ts
+++ generator/konfig-next-app/src/utils/generate-shade-palette.ts
@@ -3,11 +3,13 @@ import chroma from 'chroma-js'
 
 export const generateShadePalette = (color: string): Tuple<string, 10> => {
   const baseColor = chroma(color)
+  const lightness = baseColor.get('hsl.l')
+  const targetIndex = Math.round(lightness * 9)
   const scale = chroma
     .scale([baseColor.brighten(1.5), baseColor, baseColor.darken(1.5)])
     .mode('lab')
   const shades = Array.from({ length: 10 }, (_, i) => {
-    if (i === 4) {
+    if (i === targetIndex) {
       return baseColor.hex()
     }
     return scale(i / 9).hex()
