722241ae6576ae358ac63c447a48b974365fd9d8
dphuang2 <dylan.p.huang@gmail.com>
2023-08-20 22:27:43 -0700

    navbar template added
    TODO: generate navbar data from openapi.yaml

---
+++ generator/konfig-next-app/src/components/ReferenceNavbar.tsx
@@ -0,0 +1,103 @@
+import {
+  Navbar,
+  Group,
+  Code,
+  ScrollArea,
+  createStyles,
+  rem,
+} from '@mantine/core'
+import { LinksGroup } from './ReferenceNavbarLinksGroup'
+
+const mockdata = [
+  { label: 'Dashboard' },
+  {
+    label: 'Market news',
+
+    initiallyOpened: true,
+    links: [
+      { label: 'Overview', link: '/' },
+      { label: 'Forecasts', link: '/' },
+      { label: 'Outlook', link: '/' },
+      { label: 'Real time', link: '/' },
+    ],
+  },
+  {
+    label: 'Releases',
+
+    links: [
+      { label: 'Upcoming releases', link: '/' },
+      { label: 'Previous releases', link: '/' },
+      { label: 'Releases schedule', link: '/' },
+    ],
+  },
+  { label: 'Analytics' },
+  { label: 'Contracts' },
+  { label: 'Settings' },
+  {
+    label: 'Security',
+    links: [
+      { label: 'Enable 2FA', link: '/' },
+      { label: 'Change password', link: '/' },
+      { label: 'Recovery codes', link: '/' },
+    ],
+  },
+]
+
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
+  },
+
+  header: {
+    padding: theme.spacing.md,
+    paddingTop: 0,
+    marginLeft: `calc(${theme.spacing.md} * -1)`,
+    marginRight: `calc(${theme.spacing.md} * -1)`,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  links: {
+    marginLeft: `calc(${theme.spacing.md} * -1)`,
+    marginRight: `calc(${theme.spacing.md} * -1)`,
+  },
+
+  linksInner: {
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+  },
+
+  footer: {
+    marginLeft: `calc(${theme.spacing.md} * -1)`,
+    marginRight: `calc(${theme.spacing.md} * -1)`,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+}))
+
+export function ReferenceNavbar() {
+  const { classes } = useStyles()
+  const links = mockdata.map((item) => (
+    <LinksGroup {...item} key={item.label} />
+  ))
+
+  return (
+    <>
+      <Navbar.Section className={classes.header}>
+        <Group position="apart">
+          <Code>api.snaptrade.com/api/v1</Code>
+          <Code>v3.1.2</Code>
+        </Group>
+      </Navbar.Section>
+
+      <Navbar.Section grow className={classes.links} component={ScrollArea}>
+        <div className={classes.linksInner}>{links}</div>
+      </Navbar.Section>
+    </>
+  )
+}
