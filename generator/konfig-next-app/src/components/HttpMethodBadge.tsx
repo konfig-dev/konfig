37a2193b34408fbd2a84dc4b45e744178a14d290
dphuang2 <dylan.p.huang@gmail.com>
2023-08-21 18:56:58 -0700

    clear redis cache for /revalidate-demos

--- generator/konfig-next-app/src/components/HttpMethodBadge.tsx
+++ generator/konfig-next-app/src/components/HttpMethodBadge.tsx
@@ -1,11 +1,23 @@
-import { Badge, MantineColor, useMantineTheme } from '@mantine/core'
+import {
+  Badge,
+  MantineColor,
+  MantineSize,
+  useMantineTheme,
+} from '@mantine/core'
 import type { HttpMethods } from 'konfig-lib'
 import { HttpMethodsEnum } from 'konfig-lib/dist/forEachOperation'
 
-export function HttpMethodBadge({ httpMethod }: { httpMethod: HttpMethods }) {
+export function HttpMethodBadge({
+  httpMethod,
+  size,
+}: {
+  httpMethod: HttpMethods
+  size?: MantineSize
+}) {
   const theme = useMantineTheme()
   return (
     <Badge
+      size={size}
       variant={theme.colorScheme === 'light' ? 'filled' : 'light'}
       color={httpMethodColor(httpMethod)}
       radius="xs"
