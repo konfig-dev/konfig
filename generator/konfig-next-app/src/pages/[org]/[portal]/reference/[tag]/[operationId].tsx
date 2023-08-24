da56fac801fffa1a24f84e9a2a0f286db9cbc803
dphuang2 <dylan.p.huang@gmail.com>
2023-08-24 10:48:12 -0700

    put [operation] reference main content inside of component

--- generator/konfig-next-app/src/pages/[org]/[portal]/reference/[tag]/[operationId].tsx
+++ generator/konfig-next-app/src/pages/[org]/[portal]/reference/[tag]/[operationId].tsx
@@ -9,16 +9,8 @@ import {
   useMantineTheme,
   Header,
   Box,
-  Title,
   Group,
-  Text,
   MantineProvider,
-  Code,
-  Stack,
-  Flex,
-  Divider,
-  Badge,
-  Button,
 } from '@mantine/core'
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 import { useState } from 'react'
@@ -28,7 +20,6 @@ import {
   githubGetReferenceResources,
 } from '@/utils/github-get-reference-resources'
 import {
-  ParameterObject,
   type OperationObject,
   Spec,
   getOperations,
@@ -37,16 +28,10 @@ import {
   ResponseObject,
   SecurityScheme,
 } from 'konfig-lib'
-import { HttpMethodBadge } from '@/components/HttpMethodBadge'
-import { OperationParameter, Parameter } from '@/components/OperationParameter'
-import { httpResponseCodeMeaning } from '@/utils/http-response-code-meaning'
+import { Parameter } from '@/components/OperationParameter'
 import { sortParametersByRequired } from '@/utils/sort-parameters-by-required'
 import { NavbarDataItem } from '@/components/LinksGroup'
-import { OperationForm } from '@/components/OperationForm'
-import { FormProvider, useForm } from '@/utils/operation-form-context'
-import { generateInitialFormValues } from '@/utils/generate-initial-operation-form-values'
-import { OperationFormGeneratedCode } from '@/components/OperationFormGeneratedCode'
-import { useRouter } from 'next/router'
+import { OperationReferenceMain } from '@/OperationReferenceMain'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
@@ -64,22 +49,22 @@ export const getStaticPaths: GetStaticPaths = async () => {
  * konfig.yaml contents and return it as a props
  */
 
-export const getStaticProps: GetStaticProps<
-  Omit<GithubResources, 'spec'> & {
-    spec: Spec['spec']
-    operationId: string
-    operation: OperationObject
-    pathParameters: Parameter[]
-    queryParameters: Parameter[]
-    headerParameters: Parameter[]
-    cookieParameters: Parameter[]
-    requestBodyProperties: Record<string, SchemaObject> | null
-    requestBodyRequired: string[] | null
-    responses: Record<string, ResponseObject>
-    securityRequirements: Record<string, string[]> | null
-    securitySchemes: Record<string, SecurityScheme> | null
-  }
-> = async (ctx) => {
+export type StaticProps = Omit<GithubResources, 'spec'> & {
+  spec: Spec['spec']
+  operationId: string
+  operation: OperationObject
+  pathParameters: Parameter[]
+  queryParameters: Parameter[]
+  headerParameters: Parameter[]
+  cookieParameters: Parameter[]
+  requestBodyProperties: Record<string, SchemaObject> | null
+  requestBodyRequired: string[] | null
+  responses: Record<string, ResponseObject>
+  securityRequirements: Record<string, string[]> | null
+  securitySchemes: Record<string, SecurityScheme> | null
+}
+
+export const getStaticProps: GetStaticProps<StaticProps> = async (ctx) => {
   const owner = ctx.params?.org
   const repo = ctx.params?.portal
   const tag = ctx.params?.tag
@@ -270,19 +255,6 @@ const Operation = ({
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { colors } = useMantineTheme()
   const { colorScheme } = useMantineColorScheme()
-  const initialValues = generateInitialFormValues({
-    parameters: [
-      ...pathParameters,
-      ...queryParameters,
-      ...headerParameters,
-      ...cookieParameters,
-    ],
-    requestBodyProperties,
-  })
-
-  const form = useForm({
-    initialValues,
-  })
 
   const [opened, setOpened] = useState(false)
   return (
@@ -339,121 +311,17 @@ const Operation = ({
           </Header>
         }
       >
-        <FormProvider form={form}>
-          <form
-            onSubmit={form.onSubmit((values) => {
-              console.log(values)
-            })}
-          >
-            <Flex justify="space-between">
-              <Stack w="55%" spacing="xl">
-                <Stack spacing="xs">
-                  <Title order={2}>
-                    {operation.operation.summary ?? operation.path}
-                  </Title>
-                  <Group>
-                    <HttpMethodBadge httpMethod={operation.method} />
-                    <Code>{operation.path}</Code>
-                  </Group>
-                  {operation.operation.description && (
-                    <Text c="dimmed" fz="sm">
-                      {operation.operation.description}
-                    </Text>
-                  )}
-                </Stack>
-                <OperationForm
-                  pathParameters={pathParameters}
-                  queryParameters={queryParameters}
-                  headerParameters={headerParameters}
-                  cookieParameters={cookieParameters}
-                  requestBodyProperties={requestBodyProperties}
-                  requestBodyRequired={requestBodyRequired}
-                />
-                {responses && (
-                  <Box>
-                    <Title order={4}>Responses</Title>
-                    <Divider my="sm" />
-                    <Stack>
-                      {Object.entries(responses).map(
-                        ([responseCode, response]) => (
-                          <Box key={responseCode}>
-                            {/* 1. Render response code
-                          2. Render meaning of response code like "OK" for 200 and "Not Found" for 404 in same text box as (1)
-                          3. Render green "Success" badge next to 2xx codes and red "Error" badge next to 4xx and 5xx codes
-                          4. Render response description if it exists under the response code + badge
-                       */}
-
-                            <Flex gap="xs" align="center">
-                              <Title order={6}>
-                                {responseCode}{' '}
-                                {httpResponseCodeMeaning(responseCode)}
-                              </Title>
-                              {responseCode.startsWith('2') ? (
-                                <Badge
-                                  variant={
-                                    colorScheme === 'dark' ? 'light' : 'filled'
-                                  }
-                                  radius="xs"
-                                  color="teal"
-                                  size="xs"
-                                >
-                                  Success
-                                </Badge>
-                              ) : (
-                                <Badge
-                                  variant={
-                                    colorScheme === 'dark' ? 'light' : 'filled'
-                                  }
-                                  radius="xs"
-                                  color="red"
-                                  size="xs"
-                                >
-                                  Error
-                                </Badge>
-                              )}
-                            </Flex>
-                            {response.description && (
-                              <Text c="dimmed" fz="sm">
-                                {response.description}
-                              </Text>
-                            )}
-                          </Box>
-                        )
-                      )}
-                    </Stack>
-                  </Box>
-                )}
-              </Stack>
-              <Box w="40%">
-                <Stack
-                  pos="sticky"
-                  top="calc(var(--mantine-header-height, 0px) + 1rem)"
-                  w="100%"
-                  spacing="sm"
-                >
-                  {securitySchemes && (
-                    <>
-                      <Title order={4}> Authorization </Title>
-                      {Object.entries(securitySchemes).map(
-                        ([name, scheme]) => {}
-                      )}
-                    </>
-                  )}
-                  <OperationFormGeneratedCode
-                    operation={operation.operation}
-                    values={form.values}
-                  />
-                  <Button
-                    variant={colorScheme === 'dark' ? 'light' : 'filled'}
-                    type="submit"
-                  >
-                    Request API
-                  </Button>
-                </Stack>
-              </Box>
-            </Flex>
-          </form>
-        </FormProvider>
+        <OperationReferenceMain
+          pathParameters={pathParameters}
+          queryParameters={queryParameters}
+          headerParameters={headerParameters}
+          cookieParameters={cookieParameters}
+          requestBodyProperties={requestBodyProperties}
+          requestBodyRequired={requestBodyRequired}
+          responses={responses}
+          securitySchemes={securitySchemes}
+          operation={operation}
+        />
       </AppShell>
     </MantineProvider>
   )
