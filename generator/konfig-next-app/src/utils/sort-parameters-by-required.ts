fe3173f9004d6ed990ce6abe6ad53e72c7665196
dphuang2 <dylan@konfigthis.com>
2023-08-26 03:17:19 -0700

    Update sort-parameters-by-required.ts

--- generator/konfig-next-app/src/utils/sort-parameters-by-required.ts
+++ generator/konfig-next-app/src/utils/sort-parameters-by-required.ts
@@ -1,5 +1,6 @@
 import { Parameter } from '@/components/OperationParameter'
 
+// to ensure required parameters always show up first in the UI
 export function sortParametersByRequired(parameters: Parameter[]): Parameter[] {
   return parameters.sort((a, b) => {
     if (a.required && !b.required) {
