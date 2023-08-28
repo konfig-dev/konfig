fb826f72fa7904cbdbc1cf1c1a88f3fff5c985b4
dphuang2 <dylan.p.huang@gmail.com>
2023-08-28 12:59:51 -0700

    pluralize

--- generator/konfig-next-app/src/utils/schema-type-label.ts
+++ generator/konfig-next-app/src/utils/schema-type-label.ts
@@ -1,7 +1,11 @@
 import { SchemaObject } from 'konfig-lib'
+import { pluralize } from './pluralize'
 
 export function schemaTypeLabel({ schema }: { schema: SchemaObject }) {
   const type = schema.type && schema.type
   const nullable = 'nullable' in schema && schema.nullable ? ' or null' : ''
-  return `${type}${nullable}`
+  const innerType =
+    schema.type === 'array' ? (schema.items as SchemaObject).type : null
+  const arrayOf = innerType !== null ? ` of ${pluralize(innerType)}` : ''
+  return `${type}${arrayOf}${nullable}`
 }
