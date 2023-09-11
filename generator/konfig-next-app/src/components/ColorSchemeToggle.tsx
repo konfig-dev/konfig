d6fafd25edbe5a52bf93d6f3e640969ad3c129d4
dphuang2 <dylan.p.huang@gmail.com>
2023-09-10 21:35:42 -0700

    fix color scheme toggle sstyling

--- generator/konfig-next-app/src/components/ColorSchemeToggle.tsx
+++ generator/konfig-next-app/src/components/ColorSchemeToggle.tsx
@@ -1,10 +1,20 @@
-import { ActionIcon, useMantineColorScheme } from '@mantine/core'
+import {
+  ActionIcon,
+  useMantineColorScheme,
+  useMantineTheme,
+} from '@mantine/core'
 import { IconSun, IconMoonStars } from '@tabler/icons-react'
 
 export function ColorSchemeToggle() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+  const theme = useMantineTheme()
   return (
-    <ActionIcon variant="filled" onClick={() => toggleColorScheme()} size={25}>
+    <ActionIcon
+      color={colorScheme === 'light' ? 'brand' : undefined}
+      variant={colorScheme === 'light' ? 'filled' : 'light'}
+      onClick={() => toggleColorScheme()}
+      size={25}
+    >
       {colorScheme === 'dark' ? (
         <IconSun size="1rem" />
       ) : (
