b4ae46e4200d44acd4e53b10d78d268a48071ab6
dphuang2 <dylan.p.huang@gmail.com>
2023-09-14 01:18:42 -0700

    TODO DocumentationPage

---
+++ generator/konfig-next-app/src/pages/[org]/[portal]/docs/[[...id]].tsx
@@ -0,0 +1,7 @@
+// NextJS page definition that accepts any number of path segments as a catch-all route
+
+const DocumentationPage = () => {
+  return <div>test</div>
+}
+
+export default DocumentationPage
