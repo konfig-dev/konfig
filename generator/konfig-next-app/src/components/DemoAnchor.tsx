c7c72d099d2b34e65075bb454715344986fd9b3b
dphuang2 <dylan.p.huang@gmail.com>
2023-09-14 23:46:11 -0700

    add flare for demo and api links

--- generator/konfig-next-app/src/components/DemoAnchor.tsx
+++ generator/konfig-next-app/src/components/DemoAnchor.tsx
@@ -1,7 +1,8 @@
 import { observer } from 'mobx-react'
 import { Components } from 'react-markdown'
-import { useMantineTheme, Text } from '@mantine/core'
+import { useMantineTheme, Text, Button, rem } from '@mantine/core'
 import Link from 'next/link'
+import { IconCode, IconTerminal } from '@tabler/icons-react'
 
 const _DemoAnchor: Components['a'] = ({
   node,
@@ -18,6 +19,43 @@ const _DemoAnchor: Components['a'] = ({
         {children}
       </Text>
     )
+  if (href?.includes('/demo/')) {
+    return (
+      <Link
+        href={href ?? '#'}
+        style={{ color: colors.brand[7], textDecoration: 'none' }}
+        {...props}
+      >
+        <Button
+          leftIcon={<IconTerminal size={rem(12)} />}
+          variant="light"
+          compact
+          size="sm"
+        >
+          {children}
+        </Button>
+      </Link>
+    )
+  }
+
+  if (href?.includes('/reference/')) {
+    return (
+      <Link
+        href={href ?? '#'}
+        style={{ color: colors.brand[7], textDecoration: 'none' }}
+        {...props}
+      >
+        <Button
+          leftIcon={<IconCode size={rem(12)} />}
+          variant="light"
+          compact
+          size="sm"
+        >
+          {children}
+        </Button>
+      </Link>
+    )
+  }
   return (
     <Link
       href={href ?? '#'}
