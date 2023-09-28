1d67e1c490095f8818364850c98fb91f286588ba
eddiechayes <eddiechayes@gmail.com>
2023-09-28 15:32:33 -0700

    sanity check without exclude

--- generator/konfig-dash/bash-scripts/guard-api-key-leak.sh
+++ generator/konfig-dash/bash-scripts/guard-api-key-leak.sh
@@ -8,12 +8,7 @@ searchFolder="../../.." # entire repo
 foundKey=false
 
 for apiKey in "${apiKeys[@]}"; do
-  files=$(grep -rl "$apiKey" "$searchFolder" \
-    --exclude=../../../generator/konfig-dash/.redwood/prebuild/api/src/lib/api-keys.js \
-    --exclude=../../../generator/konfig-dash/api/dist/lib/api-keys.js.map \
-    --exclude=../../../generator/konfig-dash/api/dist/lib/api-keys.js \
-    --exclude=../../../generator/konfig-dash/api/src/lib/api-keys.ts
-  )
+  files=$(grep -rl "$apiKey" "$searchFolder")
   if [ -n "$files" ]; then
     foundKey=true
     echo "ERROR: Security risk detected. API key leaked in the following file(s):"
@@ -22,7 +17,7 @@ for apiKey in "${apiKeys[@]}"; do
   fi
 done
 
-if "$foundKey"; then
+if [ "$foundKey" ]; then
   exit 1
 fi
 echo No api key leaks detected.
