e21a9fd41529705e30f3f0a8b085a1976e1426bc
eddiechayes <eddiechayes@gmail.com>
2023-09-28 16:35:47 -0700

    test exclude

--- generator/konfig-dash/bash-scripts/guard-api-key-leak.sh
+++ generator/konfig-dash/bash-scripts/guard-api-key-leak.sh
@@ -10,7 +10,7 @@ searchFolder="../../.." # entire repo
 foundKey=false
 
 for apiKey in "${apiKeys[@]}"; do
-  files=$(grep -rl "$apiKey" "$searchFolder")
+  files=$(grep -rl "$apiKey" "$searchFolder" --exclude "$keyFile")
   if [ -n "$files" ]; then
     foundKey=true
     echo "ERROR: Security risk detected. API key leaked in the following file(s):"
