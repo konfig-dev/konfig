f65a7ac3c1601e51d72f3b391ff355b82e94a0a2
eddiechayes <eddiechayes@gmail.com>
2023-09-28 14:08:33 -0700

    yarn build in workflow. search whole repo in script

--- generator/konfig-dash/bash-scripts/guard-api-key-leak.sh
+++ generator/konfig-dash/bash-scripts/guard-api-key-leak.sh
@@ -4,11 +4,15 @@
 keyFile="../api/src/lib/api-keys.ts"
 apiKeys=($(grep -oE '[A-Za-z0-9]{32}:' "$keyFile" | sed 's/://'))
 
-searchFolder="../packages"
+searchFolder="../../.." # entire repo
 foundKey=false
 
 for apiKey in "${apiKeys[@]}"; do
-  files=$(grep -rl "$apiKey" "$searchFolder")
+  files=$(grep -rl "$apiKey" "$searchFolder" \
+    --exclude=../../../generator/konfig-dash/.redwood/prebuild/api/src/lib/api-keys.js \
+    --exclude=../../../generator/konfig-dash/api/dist/lib/api-keys.js.map \
+    --exclude=../../../generator/konfig-dash/api/dist/lib/api-keys.js \
+    --exclude=../../../generator/konfig-dash/api/src/lib/api-keys.ts)
   if [ -n "$files" ]; then
     foundKey=true
     echo "ERROR: Security risk detected. API key leaked in the following file(s):"
