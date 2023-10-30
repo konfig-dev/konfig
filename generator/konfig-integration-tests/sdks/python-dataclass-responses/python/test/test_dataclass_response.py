fc0238ecb840cd128bcab35d9f524757fbb8a1cc
dphuang2 <dylan.p.huang@gmail.com>
2023-10-30 16:46:58 -0700

    fix test

--- generator/konfig-integration-tests/sdks/python-dataclass-responses/python/test/test_dataclass_response.py
+++ generator/konfig-integration-tests/sdks/python-dataclass-responses/python/test/test_dataclass_response.py
@@ -15,7 +15,7 @@ class TestSimple(unittest.TestCase):
 
     def test_response_is_dataclass(self):
         pythondataclassresponses = PythonDataclassResponses(
-            api_key="YOUR_API_KEY", host="http://127.0.0.1:4010"
+            api_key="YOUR_API_KEY", host="http://127.0.0.1:4011"
         )
         response = pythondataclassresponses.test.fetch("input")
         self.assertTrue(is_dataclass(response))
