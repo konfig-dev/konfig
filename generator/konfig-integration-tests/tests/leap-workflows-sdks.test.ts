0a1b7981b0aaadffdd1f0f954dbac1c93dd964b7
eddiechayes <eddiechayes@gmail.com>
2023-10-30 11:53:59 -0700

    Earthly Local CI

--- generator/konfig-integration-tests/tests/leap-workflows-sdks.test.ts
+++ generator/konfig-integration-tests/tests/leap-workflows-sdks.test.ts
@@ -2,4 +2,4 @@ import { e2e } from "../util";
 
 test("leap-workflows-sdks", async () => {
   await e2e(4010);
-});
+}, 900000);
--- generator/konfig-integration-tests/tests/python-typeddict-responses.test.ts
+++ generator/konfig-integration-tests/tests/python-typeddict-responses.test.ts
@@ -2,4 +2,4 @@ import { e2e } from "../util";
 
 test("python-typeddict-responses", async () => {
   await e2e(4012);
-});
+}, 900000);
