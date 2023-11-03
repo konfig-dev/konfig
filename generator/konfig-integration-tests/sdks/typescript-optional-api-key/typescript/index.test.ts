f1167a499f714bd568ff7bb15924815234e224b6
eddiechayes <eddiechayes@gmail.com>
2023-11-02 17:35:46 -0700

    test SDK

---
+++ generator/konfig-integration-tests/sdks/typescript-optional-api-key/typescript/index.test.ts
@@ -0,0 +1,9 @@
+import { TypescriptOptionalApiKey } from "./index";
+
+describe("typescript-optional-api-key", () => {
+    it("initialize client", async () => {
+        const typescriptoptionalapikey = new TypescriptOptionalApiKey({
+            apiKey: "API_KEY",
+        });
+    });
+});
