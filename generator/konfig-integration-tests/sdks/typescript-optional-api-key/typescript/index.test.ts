05dbd358ba40b924d59fd9a2c1cc870736e19ae2
eddiechayes <eddiechayes@gmail.com>
2023-11-02 18:02:20 -0700

    ensure client can be instantiated without apikey

--- generator/konfig-integration-tests/sdks/typescript-optional-api-key/typescript/index.test.ts
+++ generator/konfig-integration-tests/sdks/typescript-optional-api-key/typescript/index.test.ts
@@ -1,9 +1,13 @@
 import { TypescriptOptionalApiKey } from "./index";
 
 describe("typescript-optional-api-key", () => {
-    it("initialize client", async () => {
-        const typescriptoptionalapikey = new TypescriptOptionalApiKey({
-            apiKey: "API_KEY",
-        });
+  it("initialize client with api key", async () => {
+    const typescriptoptionalapikey = new TypescriptOptionalApiKey({
+      apiKey: "API_KEY",
     });
+  });
+
+  it("initialize client without api key", async () => {
+    const typescriptoptionalapikey = new TypescriptOptionalApiKey({});
+  });
 });
