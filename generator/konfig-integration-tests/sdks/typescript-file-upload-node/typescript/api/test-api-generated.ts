cac80a3c662d9bd6562c41fe512b472848739c1f
dphuang2 <dylan.p.huang@gmail.com>
2023-11-08 15:29:56 -0800

    add test for buffer and File

--- generator/konfig-integration-tests/sdks/typescript-file-upload-node/typescript/api/test-api-generated.ts
+++ generator/konfig-integration-tests/sdks/typescript-file-upload-node/typescript/api/test-api-generated.ts
@@ -55,13 +55,25 @@ export const TestApiAxiosParamCreator = function (configuration?: Configuration)
             const addFormParam = async (name: string, data: any, isBinary: boolean, isPrimitiveType: boolean) => {
                 if (isBinary) {
                     if (data instanceof Uint8Array) {
-                        // Node.js
+                        // Handle Buffer data
                         const filetype = await fromBuffer(data)
                         const filename = filetype === undefined ? name : `${name}.${filetype.ext}`
                         localVarFormParams.append(name, data as any, filename);
                     } else if ("name" in data) {
-                        // Browser
-                        localVarFormParams.append(name, data as any, data.name);
+                        // File instances in browsers and Node.js have the
+                        // "name" property "Duck typing" files to handle browser
+                        // File class or Node.js File class
+                        // Web: https://developer.mozilla.org/en-US/docs/Web/API/File
+                        // Node.js: https://nodejs.org/api/buffer.html#new-bufferfilesources-filename-options
+                        if (isBrowser()) {
+                            // FormData in browser can accept File/Blob directly
+                            localVarFormParams.append(name, data, data.name);
+                        } else {
+                            // FormData in Node.js can only accept raw Buffer so convert before passing
+                            const bytes = await data.arrayBuffer()
+                            const buffer = Buffer.from(bytes)
+                            localVarFormParams.append(name, buffer, data.name);
+                        }
                     }
                 } else {
                     if (isPrimitiveType) {
--- generator/konfig-integration-tests/sdks/typescript-file-upload-node/typescript/index.test.ts
+++ generator/konfig-integration-tests/sdks/typescript-file-upload-node/typescript/index.test.ts
@@ -7,13 +7,23 @@ describe("typescript-file-upload-node", () => {
       apiKey: "API_KEY",
     });
   });
-  it("file upload", async () => {
+  it("file upload - file", async () => {
     const client = new TypescriptFileUploadNodeClient({
-      basePath: "http:/127.0.0.1:4014",
+      basePath: "http://127.0.0.1:4014",
       apiKey: "API_KEY",
     });
     const result = await client.test.fetch({
       file: new buffer.File([], "file.txt"),
+    });
+    expect(result).not.toBeNull();
+  });
+  it("file upload - buffer", async () => {
+    const client = new TypescriptFileUploadNodeClient({
+      basePath: "http://127.0.0.1:4014",
+      apiKey: "API_KEY",
+    });
+    const result = await client.test.fetch({
+      file: Buffer.from(await (new buffer.File([], "file.txt").arrayBuffer())),
     });
     expect(result).not.toBeNull();
   });
