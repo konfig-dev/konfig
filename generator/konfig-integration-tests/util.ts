d8be898dbe36967f8113210d8eafa2d7cd855041
dphuang2 <dylan.p.huang@gmail.com>
2023-12-02 13:32:00 -0800

    Revert "remove stdio"
    This reverts commit 59c37927e21b2274a2aa3c7ef72533b29026aa45.

--- generator/konfig-integration-tests/util.ts
+++ generator/konfig-integration-tests/util.ts
@@ -1,4 +1,4 @@
-import { execa } from "execa";
+import { ExecaChildProcess, execa } from "execa";
 import * as path from "path";
 import yaml from "js-yaml";
 import { expect } from "vitest";
@@ -188,6 +188,7 @@ async function callAndLogExeca(command: string, args: string[], options: any) {
   console.log(`Running: ${command} ${args.join(" ")}`);
   await execa(command, args, {
     ...options,
+    stdio: "inherit",
   });
 }
 
