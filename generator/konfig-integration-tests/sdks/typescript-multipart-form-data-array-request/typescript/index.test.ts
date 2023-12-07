f16a33df43aa3bd914dc6757ad473a6957de159c
dphuang2 <dylan.p.huang@gmail.com>
2023-12-06 17:26:39 -0800

    boostrap test for testing multipart form data with array

---
+++ generator/konfig-integration-tests/sdks/typescript-multipart-form-data-array-request/typescript/index.test.ts
@@ -0,0 +1,9 @@
+import { TypescriptMultipartFormDataArrayRequestClient } from "./index";
+
+describe("typescript-multipart-form-data-array-request", () => {
+    it("initialize client", async () => {
+        const typescriptmultipartformdataarrayrequestclient = new TypescriptMultipartFormDataArrayRequestClient({
+            apiKey: "API_KEY",
+        });
+    });
+});
---
+++ generator/konfig-integration-tests/tests/typescript-multipart-form-data-array-request.test.ts
@@ -0,0 +1,6 @@
+import { e2e } from "../util";
+import { test } from "vitest";
+
+test("typescript-multipart-form-data-array-request", async () => {
+  await e2e(4083);
+});
