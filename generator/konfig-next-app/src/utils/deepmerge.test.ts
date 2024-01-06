c93214400127e8d519f4cfd494f5b3d8b141f8c3
dphuang2 <dylan.p.huang@gmail.com>
2024-01-05 16:15:46 -0800

    delete unnecessary tests

--- generator/konfig-next-app/src/utils/deepmerge.test.ts
+++ generator/konfig-next-app/src/utils/deepmerge.test.ts
@@ -1,44 +1,6 @@
 import { test, expect } from 'vitest'
 import { deepmerge } from './deepmerge'
 
-test.skip('deepmerge should prioritize non-empty requestBody', () => {
-  const firstArg = {
-    requestBody: [
-      {
-        blob: '',
-        metadata: {
-          bucketId: '',
-          fileName: '',
-          fileType: '',
-          searchData: '',
-        },
-      },
-    ],
-  }
-
-  const secondArg = {
-    requestBody: '',
-  }
-
-  const result = deepmerge(firstArg, secondArg as any)
-
-  // Ensure that the requestBody prioritizes the argument that is not an empty string
-  expect(result.requestBody).toEqual(firstArg.requestBody)
-})
-
-test('deepmerge should not result in undefined parameters', () => {
-  const arg = {
-    parameters: {
-      hello: 'world',
-    },
-  }
-
-  const result = deepmerge(arg, arg)
-
-  // Ensure that the parameters property is not undefined
-  expect(result.parameters).not.toBeUndefined()
-})
-
 test('deepmerge should not concatenate arrays', () => {
   const firstArg = {
     requestBody: [
