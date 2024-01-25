d54770213db3d3057dfa13537fda0153fc723439
dphuang2 <dylan.p.huang@gmail.com>
2024-01-25 13:09:49 -0800

    initialize "python-union-string-discriminator"

---
+++ generator/konfig-integration-tests/sdks/python-union-string-discriminator/python/test/test_python_union_string_discriminator.py
@@ -0,0 +1,24 @@
+import unittest
+
+import os
+from pprint import pprint
+from python_union_string_discriminator import PythonUnionStringDiscriminatorClient
+
+class TestPythonUnionStringDiscriminator(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_python_union_string_discriminator(self):
+        client = PythonUnionStringDiscriminatorClient(
+            host="http://127.0.0.1:4050",
+            api_key='YOUR_API_KEY',
+        )
+        resp = client.test.fetch()
+        self.assertIsNotNone(resp)
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ generator/konfig-integration-tests/tests/python-union-string-discriminator.test.ts
@@ -0,0 +1,6 @@
+import { e2e } from "../util";
+import { test } from "vitest";
+
+test("python-union-string-discriminator", async () => {
+  await e2e(4050);
+});
