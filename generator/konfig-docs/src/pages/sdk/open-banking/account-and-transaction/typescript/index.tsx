0fd9415cbf7e4ece58862ce74d7c55aba6d78d98
dphuang2 <dylan.p.huang@gmail.com>
2024-01-29 16:38:12 -0800

    add whatsapp

--- generator/konfig-docs/src/pages/sdk/open-banking/account-and-transaction/typescript/index.tsx
+++ generator/konfig-docs/src/pages/sdk/open-banking/account-and-transaction/typescript/index.tsx
@@ -16,7 +16,7 @@ export default function OpenBankingAccountAndTransactionTypeScriptSdk() {
       logo="https://raw.githubusercontent.com/konfig-sdks/openapi-examples/HEAD/open-banking/account-and-transaction/logo.svg"
       clientNameCamelCase="openBanking"
       homepage="openbanking.org.uk"
-      lastUpdated={new Date("2024-01-30T00:11:27.494Z")}
+      lastUpdated={new Date("2024-01-30T00:35:27.195Z")}
       // Missing contactUrl
       contactEmail="ServiceDesk@openbanking.org.uk"
       previewLinkImage="https://raw.githubusercontent.com/konfig-sdks/openapi-examples/HEAD/open-banking/account-and-transaction/imagePreview.png"
