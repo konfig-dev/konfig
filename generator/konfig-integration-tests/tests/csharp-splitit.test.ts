00a054daeb3f7372df217304f31e526012a1b4e0
eddiechayes <eddiechayes@gmail.com>
2024-01-30 16:41:28 -0800

    Move handwritten splitit tests to integration tests with custom mock server

--- generator/konfig-integration-tests/tests/csharp-splitit.test.ts
+++ generator/konfig-integration-tests/tests/csharp-splitit.test.ts
@@ -2,5 +2,22 @@ import { e2e } from "../util";
 import { test } from "vitest";
 
 test("splitit/csharp-splitit", async () => {
-  await e2e(4008);
+  await e2e(4008, {
+    customServer: {
+      routes: [
+        {
+          path: "/api/installmentplans/initiate",
+          method: "post",
+          response: {
+            InstallmentPlanNumber: "string",
+            RefOrderNumber: "string",
+            PurchaseMethod: "InStore",
+            Status: "Initialized",
+            Currency: "string",
+            Amount: 0,
+          },
+        },
+      ],
+    },
+  });
 });
--- generator/konfig-integration-tests/tests/python-splitit.test.ts
+++ generator/konfig-integration-tests/tests/python-splitit.test.ts
@@ -2,5 +2,15 @@ import { e2e } from "../util";
 import { test } from "vitest";
 
 test("splitit/python-splitit", async () => {
-  await e2e(4021);
+  await e2e(4021, {
+    customServer: {
+      routes: [
+        {
+          path: "/api/installmentplans/initiate",
+          method: "post",
+          response: {},
+        },
+      ],
+    },
+  });
 });
