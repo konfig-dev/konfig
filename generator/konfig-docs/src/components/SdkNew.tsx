6adc44e104bbe0163bd37ee282c47e6958aaf083
dphuang2 <dylan@konfigthis.com>
2024-03-13 11:10:31 -0700

    extract Breadcrumbs to a component

--- generator/konfig-docs/src/components/SdkNew.tsx
+++ generator/konfig-docs/src/components/SdkNew.tsx
@@ -21,6 +21,7 @@ import { useSdkSignup } from "../util/use-sdk-signup";
 import clsx from "clsx";
 import { LoadingIcon } from "./LoadingIcon";
 import { Method } from "./SdkComponentProps";
+import { Breadcrumbs } from "./Breadcrumbs";
 
 type InputPropsFromOriginalSdkComponent = React.ComponentProps<typeof Sdk>;
 
@@ -540,73 +541,4 @@ function SignupForm({
       </div>
     </form>
   );
-}
-
-/**
- * Responsive breadcrumbs for the pSEO page.
- */
-function Breadcrumbs({
-  company,
-  language,
-  serviceName,
-}: {
-  company: string;
-  language: string;
-  serviceName?: string;
-}) {
-  return (
-    <div className="text-white mx-auto px-3 max-w-[375px] sm:max-w-none sm:px-8 text-lg font-semibold">
-      {/* Desktop */}
-      <nav className="hidden lg:block">
-        <ol className="px-0 flex gap-1 list-none items-center">
-          <li>
-            <BreadcrumbLink href="/sdk">SDKs</BreadcrumbLink>
-          </li>
-          <BreadcrumbSeparator />
-          <li>
-            <BreadcrumbNonLink>{company}</BreadcrumbNonLink>
-          </li>
-          <BreadcrumbSeparator />
-          {serviceName && (
-            <>
-              <li>
-                <BreadcrumbNonLink>{serviceName}</BreadcrumbNonLink>
-              </li>
-              <BreadcrumbSeparator />
-            </>
-          )}
-          <li>
-            <BreadcrumbNonLink>{language}</BreadcrumbNonLink>
-          </li>
-        </ol>
-      </nav>
-      {/* Mobile */}
-      <nav className="sm:mx-auto w-fit flex gap-1 lg:hidden">
-        <BreadcrumbNonLink>{company}</BreadcrumbNonLink>+
-        <BreadcrumbNonLink>{language}</BreadcrumbNonLink>
-      </nav>
-    </div>
-  );
-}
-
-function BreadcrumbLink({
-  children,
-  href,
-}: PropsWithChildren<{ href: string }>) {
-  return (
-    <a
-      className="text-white border-b border-b-blue-200 hover:border-b-2 hover:no-underline hover:text-white"
-      href={href}
-    >
-      {children}
-    </a>
-  );
-}
-
-function BreadcrumbNonLink({ children }: PropsWithChildren<{}>) {
-  return <span className="text-blue-200">{children}</span>;
-}
-
-function BreadcrumbSeparator() {
-  return <IconChevronRight className="h-5 text-blue-300" />;
 }
