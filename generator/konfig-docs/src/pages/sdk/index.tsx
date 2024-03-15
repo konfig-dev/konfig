24808bcffb107b7644171a8b04f08a69f82c8c06
dphuang2 <dylan@konfigthis.com>
2024-03-14 17:06:01 -0700

    category components sort of fleshed out

--- generator/konfig-docs/src/pages/sdk/index.tsx
+++ generator/konfig-docs/src/pages/sdk/index.tsx
@@ -2,7 +2,7 @@ import Head from "@docusaurus/Head";
 import Layout from "@theme/Layout";
 import React, { useState } from "react";
 import sdkLinksJson from "./sdk-links.json";
-import { ArrowDown, ArrowRight, ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 import {
   Collapsible,
@@ -34,7 +34,14 @@ export default function Sdks() {
       </Head>
       <div className="px-3 py-8 md:px-8 md:max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row gap-4 items-start">
-          <CategoryFilters />
+          <CategoryFilters
+            categories={{
+              "Artificial Intelligence": [
+                "Computer Vision",
+                "Natural Language Processing",
+              ],
+            }}
+          />
           <div>
             <div>search</div>
             <div>1 - 22 of 7063 APIs</div>
@@ -49,45 +56,65 @@ export default function Sdks() {
   );
 }
 
-function CategoryFilters() {
+type CategoryFiltersProps = {
+  categories: Record<string, string[]>;
+};
+
+function CategoryFilters({ categories }: CategoryFiltersProps) {
   return (
     <div>
-      <h2>Categories</h2>
+      <h3>API Categories</h3>
       <ul className="pl-0 mb-0 list-none">
-        <Category />
+        {Object.entries(categories).map(([parentCategory, categories]) => (
+          <Category parentCategory={parentCategory} categories={categories} />
+        ))}
       </ul>
     </div>
   );
 }
 
-function Category() {
+type CategoryProps = {
+  parentCategory: string;
+  categories: string[];
+};
+function Category({ parentCategory, categories }: CategoryProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Collapsible
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      className="w-[350px] space-y-2"
-    >
-      <CollapsibleTrigger className="group">
+    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="w-[350px]">
+      <CollapsibleTrigger className="group text-slate-500 hover:text-slate-800 rounded-md py-2 px-2 w-full hover:bg-slate-100 transition-all">
         <li>
-          <div className="flex items-center justify-between gap-x-2">
+          <div className="flex items-center gap-x-2">
             <ChevronDown
               className={`text-slate-400 group-aria-expanded:-rotate-180 duration-150 transition-transform`}
             />
-            <div className="font-semibold">Artificial Intelligence</div>
+            <div className="font-semibold">{parentCategory}</div>
           </div>
         </li>
       </CollapsibleTrigger>
-      <CollapsibleContent>
+      <CollapsibleContent className="mt-0">
         <ul className="mb-0 pl-0 list-none">
-          <li>
-            <a className="pl-12 block " href="">
-              API 1
-            </a>
-          </li>
+          {categories.map((category) => (
+            <SubCategory category={category} />
+          ))}
         </ul>
       </CollapsibleContent>
     </Collapsible>
+  );
+}
+
+type SubCategoryProps = {
+  category: string;
+};
+function SubCategory({ category }: SubCategoryProps) {
+  return (
+    <li>
+      <a
+        className="pl-12 rounded-md text-slate-500 hover:text-slate-800 hover:no-underline block py-2 hover:bg-slate-100 transition-all"
+        href=""
+      >
+        {category}
+      </a>
+    </li>
   );
 }
