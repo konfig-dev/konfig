63b9ee26ef0c295d79ce7d8fa554fec61529fed1
dphuang2 <dylan@konfigthis.com>
2024-08-13 12:16:49 -0700

    update test to call getOrderImpact

--- generator/konfig-integration-tests/sdks/java-additional-null-property/sdks/java/src/test/java/com/konfigthis/client/GettingStartedTest.java
+++ generator/konfig-integration-tests/sdks/java-additional-null-property/sdks/java/src/test/java/com/konfigthis/client/GettingStartedTest.java
@@ -1,42 +1,26 @@
 package com.konfigthis.client;
 
-import com.konfigthis.client.model.Account;
-import com.konfigthis.client.model.AccountHoldings;
-import com.konfigthis.client.model.Brokerage;
-import com.konfigthis.client.model.DeleteUserResponse;
-import com.konfigthis.client.model.PortfolioGroup;
-import com.konfigthis.client.model.SnapTradeRegisterUserRequestBody;
-import com.konfigthis.client.model.Status;
-import com.konfigthis.client.model.TargetAsset;
-import com.konfigthis.client.model.UniversalSymbol;
-import com.konfigthis.client.model.UserIDandSecret;
-import org.junit.jupiter.api.Disabled;
+import com.konfigthis.client.model.ManualTradeAndImpact;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
 import static org.junit.Assert.assertNotNull;
+
+import java.util.UUID;
 
 public class GettingStartedTest {
         final String MOCK_SERVER_URL = "http://localhost:4107";
 
-        @Disabled
         @Test
-        public void setPortfolioTargets() throws ApiException {
+        public void getOrderImpact() throws ApiException {
                 Configuration configuration = new Configuration();
                 configuration.clientId = "SNAPTRADE_CLIENT_ID";
                 configuration.consumerKey = "SNAPTRADE_CONSUMER_KEY";
                 configuration.host = MOCK_SERVER_URL;
                 Snaptrade snaptrade = new Snaptrade(configuration);
-                UUID userId = UUID.randomUUID();
-                UserIDandSecret userIDandSecret = snaptrade.authentication.registerSnapTradeUser()
-                        .userId(userId.toString()).execute();
-                List<TargetAsset> targetAsset = new ArrayList<>();
-                targetAsset.add(new TargetAsset().symbol(new UniversalSymbol().symbol("AAPL")).percent(90));
-                snaptrade.authentication.deleteSnapTradeUser(userIDandSecret.getUserId()).execute();
+                String userId = UUID.randomUUID().toString();
+                String userSecret = UUID.randomUUID().toString();
+                ManualTradeAndImpact result = snaptrade.trading.getOrderImpact(userId, userSecret).execute();
+                assertNotNull(result);
         }
 
 }
