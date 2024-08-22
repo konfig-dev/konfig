e9ac4a249095a313d311902790599373dfacef96
dphuang2 <dylan@konfigthis.com>
2024-08-22 12:38:15 -0700

    test it

---
+++ generator/konfig-dash/packages/konfig-cli/src/util/fix-parameters-with-no-name.ts
@@ -0,0 +1,23 @@
+import { getOperations, resolveRef, Spec } from 'konfig-lib'
+
+export async function fixParametersWithNoName({
+  spec,
+}: {
+  spec: Spec
+}): Promise<number> {
+  let numberOfParametersWithNoNameFixed = 0
+  if (spec.spec.paths === undefined) return 0
+  const operations = getOperations({ spec: spec.spec })
+  for (const { operation } of operations) {
+    if (operation.parameters === undefined) continue
+
+    const originalLength = operation.parameters.length
+    operation.parameters = operation.parameters.filter((parameter) => {
+      parameter = resolveRef({ refOrObject: parameter, $ref: spec.$ref })
+      parameter.name !== undefined && parameter.name !== ''
+    })
+    numberOfParametersWithNoNameFixed +=
+      originalLength - operation.parameters.length
+  }
+  return numberOfParametersWithNoNameFixed
+}
